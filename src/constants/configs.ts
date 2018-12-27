1231502ca2784ab22885830a5e90c7f84c86bb06
Ji Hyeon Kim <bill.jh.kim@gmail.com>
2018-12-27 14:02:49 -0500

    remove logs

--- src/constants/configs.ts
+++ src/constants/configs.ts
@@ -3,11 +3,8 @@ import * as dotenv from "dotenv";
 const result = dotenv.config()
 
 if (result.error) {
-  console.log(result.error);
   throw result.error
 }
-
-console.log(result.parsed)
 
 export const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN;
 export const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID;
